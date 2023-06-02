a09235ea9ec6ee96efb355c92a7a24df6017b11a
anthonyprime202 <anthonyprime202@gmail.com>
2023-06-02 07:03:33 +0530

    Commit!

--- final.sh
+++ final.sh
@@ -11,8 +11,9 @@ git clone https://aur.archlinux.org/paru.git
 cd paru 
 makepkg -si
 
-sudo pacman -Syu qtile sddm alacritty discord firefox nemo lxsession lxappearance qt5ct qt6ct rofi dunst neofetch blueman network-manager-applet kdeconnect cbatticon maim neovim nitrogen
+sudo pacman -Syu ripgrep qtile sddm alacritty discord firefox nemo lxsession lxappearance qt5ct qt6ct rofi dunst neofetch blueman network-manager-applet kdeconnect cbatticon maim neovim nitrogen
 paru -S qtile-extras-git picom-git kdeconnect-indicator-git 
+pip install catppuccin
 
 sudo systemctl enable NetworkManger 
 sudo systemctl enable bluetooth
--- initial.sh
+++ initial.sh
@@ -15,7 +15,7 @@ mount --mkdir -o noatime,compress=lzo,space_cache=v2,subvol=@home /dev/nvme0n1p2
 mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
 swapon /dev/nvme0n1p3
 
-pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-zen-docs linux-firmware vim vi sudo mesa amd-ucode xf86-video-amdgpu xf86-input-libinput xorg-xinput zsh man-db man-pages texinfo btrfs-progs git grub efibootmgr os-prober plymouth networkmanager bluez bluez-utils 
+pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-zen-docs linux-firmware vim vi sudo mesa amd-ucode xf86-video-amdgpu xf86-input-libinput xorg-xinput zsh man-db man-pages texinfo btrfs-progs git grub efibootmgr os-prober plymouth networkmanager bluez bluez-utils npm python-pip
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
